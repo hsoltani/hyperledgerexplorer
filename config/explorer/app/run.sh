7a38dd109532bbbeaaf9f1a5d284dd86e7b2058e
hsoltani <hsoltani@users.noreply.github.com>
2020-10-12 09:32:52 +0330

    Update run.sh

--- config/explorer/app/run.sh
+++ config/explorer/app/run.sh
@@ -1,80 +1,83 @@
-#!/bin/bash
+#!/bin/sh
 
-#    SPDX-License-Identifier: Apache-2.0
+# parameters
+MYSQL_ROOT_PWD=${MYSQL_ROOT_PWD:-"123456"}
+MYSQL_USER=${MYSQL_USER:-""}
+MYSQL_USER_PWD=${MYSQL_USER_PWD:-""}
+MYSQL_USER_DB=${MYSQL_USER_DB:-""}
 
-set -e
+if [ ! -d "/run/mysqld" ]; then
+	mkdir -p /run/mysqld
+	chown -R mysql:mysql /run/mysqld
+fi
 
-# Print usage
-function print_help() {
-	echo "Usage: "
-	echo "    main.sh <mode> [-v]"
-	echo "        <mode> - one of 'install', 'test' or 'clean'"
-	echo "            - 'install' - install all dependencies of the project"
-	echo "            - 'test' - run unit tests of the application and client code"
-	echo "            - 'clean' - clean the project directory of installed dependencies"
-	echo "        -v - enable verbose output"
-	echo "        -h - print this message"
-}
+if [ -d /var/lib/mysql/mysql ]; then
+	echo '[i] MySQL directory already present, skipping creation'
+else
+	echo "[i] MySQL data directory not found, creating initial DBs"
 
-function do_install() {
-	VERBOSE=${VERBOSE:+-ddd}
-	npm install $VERBOSE
-	(cd app/test && npm install $VERBOSE)
-	(cd client && npm install $VERBOSE && npm run build)
-}
+	chown -R mysql:mysql /var/lib/mysql
 
-function do_test() {
-	(cd app/test && npm run test)
-	(cd client && npm run test:ci -- -u --coverage)
-}
+	# init database
+	echo 'Initializing database'
+	mysql_install_db --user=mysql > /dev/null
+	echo 'Database initialized'
 
-function do_clean() {
-	rm -rf node_modules
-	rm -rf client/node_modules client/build client/coverage
-	rm -rf app/test/node_modules
-}
+	echo "[i] MySql root password: $MYSQL_ROOT_PWD"
+	# create temp file
+	tfile=`mktemp`
+	if [ ! -f "$tfile" ]; then
+	    return 1
+	fi
 
-# Get subcommand
-SUBCOMMAND=$1
-shift
+	# save sql
+	echo "[i] Create temp file: $tfile"
+	cat << EOF > $tfile
+USE mysql;
+FLUSH PRIVILEGES;
+DELETE FROM mysql.user;
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PWD' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PWD' WITH GRANT OPTION;
+EOF
 
-case $SUBCOMMAND in
-	install | test | clean)
-		;;
-	*)
-		print_help
-		exit 1
-		;;
-esac
 
-OPTIONS="hv"
-VERBOSE=
-while getopts "$OPTIONS" opt; do
-	case "$opt" in
-		v) VERBOSE=true ;;
-		h)
-			print_help
-			exit 1
-			;;
-		*)
-			echo "Unrecognized option: $opt"
-			exit 2
-			;;
-	esac
-done
+	# Create new database
+	if [ "$MYSQL_USER_DB" != "" ]; then
+		echo "[i] Creating database: $MYSQL_USER_DB"
+		echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_USER_DB\` CHARACTER SET utf8 COLLATE utf8_general_ci;" >> $tfile
 
-case $SUBCOMMAND in
-	install)
-		do_install
-		;;
-	test)
-		do_test
-		;;
-	clean)
-		do_clean
-		;;
-	*)
-		echo "Logic Error"
-		exit 3
-		;;
-esac
+		# set new User and Password
+		if [ "$MYSQL_USER" != "" ] && [ "$MYSQL_USER_PWD" != "" ]; then
+		echo "[i] Creating user: $MYSQL_USER with password $MYSQL_USER_PWD"
+		echo "GRANT ALL ON \`$MYSQL_USER_DB\`.* to '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_USER_PWD';" >> $tfile
+		fi
+	else
+		# don`t need to create new database,Set new User to control all database.
+		if [ "$MYSQL_USER" != "" ] && [ "$MYSQL_USER_PWD" != "" ]; then
+		echo "[i] Creating user: $MYSQL_USER with password $MYSQL_USER_PWD"
+		echo "GRANT ALL ON *.* to '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_USER_PWD';" >> $tfile
+		fi
+	fi
+
+	echo 'FLUSH PRIVILEGES;' >> $tfile
+
+	# run sql in tempfile
+	echo "[i] run tempfile: $tfile"
+	(cat ./blockchain-explorer/db/fabricexplorer.sql) >> $tfile
+	/usr/bin/mysqld --user=mysql --bootstrap --verbose=0 < $tfile
+	rm -f $tfile
+fi
+
+echo "[i] Sleeping 5 sec"
+sleep 5
+
+echo '[i] start running mysqld'
+exec /usr/bin/mysqld --user=mysql --console &
+
+cd blockchain-explorer
+pwd
+echo '[i] npm install'
+npm install
+rm -rf /tmp/fabric-client-kvs_peerOrg*
+echo '[i] start explorer'
+node main.js
