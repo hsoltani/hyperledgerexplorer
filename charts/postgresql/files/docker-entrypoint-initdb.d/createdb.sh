16f2fb26d8ee739007f0c90040348be6e9d8a91c
hsoltani <hsoltani@users.noreply.github.com>
2020-10-19 13:05:59 +0330

    Update createdb.sh

--- charts/postgresql/files/docker-entrypoint-initdb.d/createdb.sh
+++ charts/postgresql/files/docker-entrypoint-initdb.d/createdb.sh
@@ -21,14 +21,14 @@ case $OSTYPE in
 
 
 
-darwin*) psql postgres -v dbname=$DATABASE -v user=$USER -v passwd=$PASSWD -f ./explorerpg.sql ;
-psql postgres -v dbname=$DATABASE -v user=$USER -v passwd=$PASSWD -f ./updatepg.sql ;;
+darwin*) psql postgres -v dbname=fabricexplorer -v user=hppoc -v passwd=password -f ./explorerpg.sql ;
+psql postgres -v dbname=fabricexplorer -v user=hppoc -v passwd=password -f ./updatepg.sql ;;
 linux*)
 if [ $(id -un) = 'postgres' ]; then
   PSQL="psql"
 else
   PSQL="sudo -u postgres psql"
 fi;
-${PSQL} -v dbname=$DATABASE -v user=$USER -v passwd=$PASSWD -f ./explorerpg.sql ;
-${PSQL} -v dbname=$DATABASE -v user=$USER -v passwd=$PASSWD -f ./updatepg.sql ;;
+${PSQL} -v dbname=fabricexplorer -v user=hppoc -v passwd=password -f ./explorerpg.sql ;
+${PSQL} -v dbname=fabricexplorer -v user=hppoc -v passwd=password -f ./updatepg.sql ;;
 esac
